f1d9f70c5ee868b76a77767f7441053586a52868
Francisco Calle <francisco.calle@beeva.com>
2016-05-05 17:34:05 +0200

    Support element edition

--- client/modules/items/components/ItemProgress.jsx
+++ client/modules/items/components/ItemProgress.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Item from './Item.jsx';
 import { Row, Col } from 'react-bootstrap';
 
 const ItemList = ({ content }) => (
   <Row className="show-grid">
     <Col xs={12}>
-      <Item />
-    </Col>
+      <h1>This is the new content</h1>
+     </Col>
   </Row>
 );
 
