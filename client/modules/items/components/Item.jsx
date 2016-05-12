4e88752b965721e1644f89d489a6588d92d1a82b
Francisco Calle <francisco.calle@beeva.com>
2016-05-12 16:46:14 +0200

    Item migration

--- client/modules/items/components/Item.jsx
+++ client/modules/items/components/Item.jsx
@@ -1,49 +1,38 @@
 import React from 'react';
-import { Input, Row, Col, Panel, Glyphicon } from 'react-bootstrap';
+import Button from 'react-toolbox/lib/button';
+import { Card, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
+import Switch from 'react-toolbox/lib/switch';
 import moment from 'moment';
 
 class Item extends React.Component {
-  markComplete() {
-    const complete = this.refs.complete.getChecked();
-    const itemId = this.props.item._id;
-
-    Meteor.call('items.markComplete', complete, itemId);
+  markComplete(complete) {
+    Meteor.call('items.markComplete', complete, this.props.item._id);
   }
 
   render() {
     const { item, currentDate } = this.props;
-    const color = moment(Number(item.due)).isBefore(currentDate) ?
-      '#f00' : '#e3e3e3';
-
-    return (<Col xs={4}>
-      <Panel style={{ border: `solid 1px ${color}` }}>
-        <Row>
-          <Col xs={10}><h2>{ item.name }</h2></Col>
-          <Col xs={2}>
-            <a href={`/edit/${ item._id }`}><Glyphicon glyph="pencil" /></a>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <p>{item.description}</p>
-            <p>
-              {item.due ? moment(Number(item.due)).format('MM/DD/YYYY') : ''}
-            </p>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col xs={12}>
-            <Input
-              ref="complete"
-              type="checkbox"
-              label="Complete?"
-              onChange={this.markComplete.bind(this)}
-            />
-          </Col>
-        </Row>
-      </Panel>
-    </Col>);
+    const color =
+      (item.due ? moment(item.due) : moment()).isBefore(currentDate) ?
+        'red' : '';
+    return (<Card style={{ width: '350px' }} raised >
+      <CardTitle
+        style={{ color }}
+        avatar="https://placeimg.com/80/80/animals"
+        title={ item.name }
+        subtitle={moment(item.due).format('dddd, MMMM Do YYYY')}
+      />
+      <CardText>{item.description}</CardText>
+      <CardText>
+        <Switch
+          checked={item.complete}
+          label="Completed"
+          onChange={this.markComplete.bind(this)}
+        />
+      </CardText>
+      <CardActions>
+        <Button label="Edit" href={`/edit/${ item._id }`} icon="mode_edit" />
+      </CardActions>
+    </Card>);
   }
 }
 
