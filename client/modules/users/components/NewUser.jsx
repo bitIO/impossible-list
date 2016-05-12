4ab0e3c7fe9b1fa366bc7f902e7ca1e07f01a412
Francisco Calle <francisco.calle@beeva.com>
2016-05-12 10:03:53 +0200

    Register migration

--- client/modules/users/components/NewUser.jsx
+++ client/modules/users/components/NewUser.jsx
@@ -1,38 +1,90 @@
 import React from 'react';
-import { Col, Panel, Input, ButtonInput, Glyphicon } from 'react-bootstrap';
+import Input from 'react-toolbox/lib/input';
+import Button from 'react-toolbox/lib/button';
+import Snackbar from 'react-toolbox/lib/snackbar';
 
 class NewUser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+    };
+    this.createUser = this.createUser.bind(this);
+  }
+
   createUser(e) {
     e.preventDefault();
+    e.stopPropagation();
     const { create } = this.props;
-    const { email, password } = this.refs;
+    const { email, password } = this.state;
 
-    create(email.getValue(), password.getValue());
-    email.getInputDOMNode().value = '';
-    password.getInputDOMNode().value = '';
+    create(email, password);
+
+    this.setState({ email: '', password: '' });
+  }
+
+  handleChange(name, value) {
+    this.setState({ ...this.state, [name]: value });
+  }
+
+
+  renderError() {
+    if (this.props.error) {
+      return (<Snackbar
+        action="Dismiss"
+        active={true}
+        icon="question_answer"
+        label={this.props.error}
+        timeout={1000}
+        ref="snackbar"
+        onTimeout={() => {
+          console.log('onTimeout', this);
+          this.refs.snackbar.setState({
+            ...this.refs.snackbar.state,
+            active: false,
+          });
+        }}
+        onClick={() => {
+          this.refs.snackbar.setState({
+            ...this.refs.snackbar.state,
+            active: false,
+          });
+        }}
+        type="cancel"
+      />);
+    }
   }
 
   render() {
-    const { error } = this.props;
-    return (<Col xs={12} sm={6} smOffset={3}>
-      <Panel>
-        <h1>Register</h1>
-        {error ? <p style={{ color: 'red' }}>{ error }</p> : null}
-        <form>
-          <Input ref="email" type="email" placeholder="Email" />
-          <Input ref="password" type="password" placeholder="Password" />
-          <ButtonInput onClick={this.createUser.bind(this)}
-            bsStyle="primary" type="submit" value="Sign Up"
-          />
-        </form>
-      </Panel>
-    </Col>);
+    return (<div>
+      <h5>Register</h5>
+      {this.renderError()}
+      <form>
+        <Input
+          type="email"
+          label="Email address"
+          icon="email"
+          value={this.state.email}
+          onChange={this.handleChange.bind(this, 'email')}
+        />
+        <Input
+          type="password"
+          label="Password"
+          icon="vpn_key"
+          value={this.state.password}
+          onChange={this.handleChange.bind(this, 'password')}
+        />
+        <Button icon="input" label="Submit" raised primary onClick={this.createUser}/>
+      </form>
+    </div>);
   }
 }
 
 NewUser.propTypes = {
-  create: React.PropTypes.object,
-  error: React.PropTypes.object,
+  create: React.PropTypes.func,
+  error: React.PropTypes.string,
 };
 
 export default NewUser;
